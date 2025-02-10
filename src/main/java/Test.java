1005f60f661d755366de59170785b95ba54f8a3e
Aibek <clesh.meder.271@gmail.com>
2025-02-10 17:45:51 +0600

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        String a= "Aibek";
+
+        System.out.println("");
+    }
+}
